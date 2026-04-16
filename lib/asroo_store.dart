0cd741a9e735b40a235c3fd54d14b4078a0c1d01
Hossam Osama <hossamosamaa63@gmail.com>
2026-04-16 23:10:56 +0200

    env creat done

--- lib/asroo_store.dart
+++ lib/asroo_store.dart
@@ -1,3 +1,4 @@
+import 'package:asroo_store/core/app/env.variable.dart';
 import 'package:flutter/material.dart';
 
 class MyApp extends StatelessWidget {
@@ -8,7 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'asroo_store',
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: EnvVariable.instance.debugMode,
       theme: ThemeData(
       ),
       home: Scaffold(appBar: AppBar(
--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,22 @@ import 'dart:io';
 import 'dart:ui_web';
 
 import 'package:asroo_store/asroo_store.dart';
+import 'package:asroo_store/core/app/env.variable.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+import 'package:flutter/services.dart';
 
 void main() async{
+  await EnvVariable.instance.init(envType: EnvTyoeEnum.dev);
   WidgetsFlutterBinding.ensureInitialized();
   Platform.isAndroid
   ? await Firebase.initializeApp(
     options: const FirebaseOptions(apiKey: "AIzaSyDHLvb812v7H0ZQ_L7X3kS5PfRhTzqtQ8M", appId: '1:326872340451:android:389a8b0dd5705685c2f2d6', messagingSenderId: '326872340451', projectId: 'asroostore-fe079')
   ):
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitDown, DeviceOrientation.portraitUp]).then((_){
   runApp(const MyApp());
+
+  });
 }
 
 
